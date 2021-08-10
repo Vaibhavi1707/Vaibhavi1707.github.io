ebbce9dbdbd2f72f48ecf676dc99a250c862d090
Vaibhavi1707 <vaibhavilokegaonkar@gmail.com>
2021-08-10 08:13:28 +0530

    Added basic page structure

---
+++ portfolio/src/Intro.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Intro.css"
+
+function Intro() {
+    return (
+        <div className = "intro">
+            
+        </div>
+    )
+}
+
+export default Intro
