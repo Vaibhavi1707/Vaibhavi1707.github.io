03435a2a614b9666f3d76254a420f4483a43a5a0
Vaibhavi1707 <vaibhavilokegaonkar@gmail.com>
2021-08-14 12:45:13 +0530

    Shaped image

--- portfolio/src/pages/About.js
+++ portfolio/src/pages/About.js
@@ -8,12 +8,15 @@ function About() {
         <div className = "about">
             <h2>About Me</h2>
             <div className = "matter">
+                <div className = "desc">
                 <p>
                     I am a third year undergraduate student pursuing Computer Science at International Institute of Information Technology, Bangalore. 
                     I am interested in design thinking and development to boost user productivity. I'm currently exploring Machine Learning.            
                 </p>
-
-                <img src = {me} alt = "me"></img>
+                </div>
+                <div>
+                <img src = {me} alt = "me" style = {{ width: "30vw", height: "30vw", borderRadius: "15vw" }}></img>
+                </div>
             </div>
         </div>
     )
--- portfolio/src/pages/Experience.js
+++ portfolio/src/pages/Experience.js
@@ -9,20 +9,39 @@ import istem from "../images/istem.jpeg";
 
 import Card from "../components/Card";
 
+import { Cards } from 'react-responsive-cards';
+
 
 function Experience() {
+    const ehrcDetails = [{
+        title: "Backend Developer, E-HRC",
+        description: "NP Lab Dashboard",
+        image: ehrc,
+        // handleOnClick: () => 
+    }]
+
+    const spAwareDetails = [{
+        title: "UI/UX Intern",
+        description: "Spiritual Awareness",
+        image: spAware,
+        // handleOnClick: () => 
+    }]
+
     return (
         <div className = "experience">
             <h2>Experience</h2>
             <div className = "experience__cards">
             {/* <h3>Internships</h3> */}
-                <Card textColor = "#0A192F" cardColor = "#8892B0" imgSrc = {ehrc} title = "Backend Developer, E-HRC" description="NP Lab Dashboard" />
-                <Card textColor = "#0A192F" cardColor = "#8892B0" imgSrc = {spAware} title = "UI/UX Intern" description="Spiritual Awareness" />
+            <Card textColor = "#0A192F" cardColor = "#8892B0" imgSrc = {ehrc} title = "Backend Developer, E-HRC" description="NP Lab Dashboard" />
+            <Card textColor = "#0A192F" cardColor = "#8892B0" imgSrc = {spAware} title = "UI/UX Intern" description="Spiritual Awareness" />
+               
             {/* <h3>Volunteering</h3> */}
-                <Card textColor = "#0A192F" cardColor = "#8892B0" imgSrc = {adv} title = "Student Advocate" description="SUSE Challenge" />
-                <Card textColor = "#0A192F" cardColor = "#8892B0" imgSrc = {sq1} title = "Core Member" description = "Induction"/>
-                <Card textColor = "#0A192F" cardColor = "#8892B0" imgSrc = {istem} title = "Volunteer" description = "IStem Hackathon"/>
+            <Card textColor = "#0A192F" cardColor = "#8892B0" imgSrc = {adv} title = "Student Advocate" description="SUSE Challenge" />
+            <Card textColor = "#0A192F" cardColor = "#8892B0" imgSrc = {sq1} title = "Core Member" description = "Induction"/>
+            <Card textColor = "#0A192F" cardColor = "#8892B0" imgSrc = {istem} title = "Volunteer" description = "IStem Hackathon"/>
+            
             </div>
+            
         </div>
     )
 }
