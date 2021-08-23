f29c220b1c541c2c2439e078c66f1198c8da13af
Vaibhavi1707 <vaibhavilokegaonkar@gmail.com>
2021-08-23 10:49:38 +0530

    Fixed build errors

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { MDBContainer, MDBFooter } from "../../node_modules/mdbreact";
+// import { MDBContainer, MDBFooter } from "../../node_modules/mdbreact";
 
 const Footer = () => {
   return (
-    <MDBFooter  className="font-small pt-4 mt-4">
-      <div className="footer-copyright text-center py-3" style = {{ padding: "30px" }}>
-        <MDBContainer fluid style = {{ color: "white" }}>
+    // <MDBFooter  className="font-small pt-4 mt-4">
+      <div style = {{ padding: "30px" }}>
+        <div fluid style = {{ color: "white" }}>
           &copy; {new Date().getFullYear()} Copyright: <a style = {{ textDecoration: null, color: "white" }} href="https://github.com/Vaibhavi1707"> Vaibhavi Lokegaonkar </a>
-        </MDBContainer>
+        </div>
       </div>
-    </MDBFooter>
+    // </MDBFooter>
   );
 }
 
