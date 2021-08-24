8ea6f356e071a158e852ee5480321f55d765bdaf
Vaibhavi1707 <vaibhavilokegaonkar@gmail.com>
2021-08-24 17:39:57 +0530

    Removed contact text

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,7 +5,6 @@ function Contact() {
     return (
         <div className = "contact">
             <h2>Let's chat</h2>
-            <p>Over coffee or projects :)</p>
             <iframe title = "contactForm" src="https://docs.google.com/forms/d/e/1FAIpQLScw80tC9lNX1sbVF0y_IwxukmkO5SKcBLcEFmD9O__NtFlrMQ/viewform?embedded=true" width="640" height="725" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
         </div>
     )
